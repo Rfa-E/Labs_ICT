28c88b23e00b0026f4b32867025e8cd6c66bb127
Rfa-e <rafael.antonio.echevarria@estudiantat.upc.edu>
2025-04-21 17:48:25 +0200

    helloworld

--- Deliverable Session 01.py
+++ Deliverable Session 01.py
@@ -1,1 +1,4 @@
-## Deliverable Session 01
+# Deliverable Session 01
+# a. (1) Create your first “helloworld”, print it on the terminal.
+# https://www.w3schools.com/python/python_intro.asp
+print("helloworld")
